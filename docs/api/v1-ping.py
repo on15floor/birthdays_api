109457505d07cba1f08fc730e649b359df42629e
on15floor <on15floor@gmail.com>
2021-09-19 16:45:11 +0300

    Add test files

---
+++ docs/api/v1-ping.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+url = 'http://localhost:8888'
+
+url += '/v1/ping'
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+if __name__ == '__main__':
+    response = requests.get(url, headers=headers)
+    print(json.dumps(json.loads(response.text), indent=4, ensure_ascii=False))
