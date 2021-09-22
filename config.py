46d401d7b1d1ff68770cd805f2c32d42cbecdd31
on15floor <on15floor@gmail.com>
2021-09-22 23:02:29 +0300

    Refactor config file. Added .env support

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # База данных
-DB_DIR = os.path.dirname(os.path.abspath(__name__))
-DB_NAME = 'db_api.db'
+DB_DIR = os.getenv('DB_DIR')
+DB_NAME = os.getenv('DB_NAME')
 
 
 class Constants:
     VERSION = '1.00'
-    DEBUG = True
+    DEBUG = os.getenv('DEBUG')
     PORT = 8888
