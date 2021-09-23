ddf829661cb6b2c47a6be9176df3c69ce1bdf810
on15floor <on15floor@gmail.com>
2021-09-23 21:33:03 +0300

    Set logging to stdout

--- web.py
+++ web.py
@@ -1,15 +1,18 @@
-from tornado.web import Application
-from tornado.ioloop import IOLoop
-from config import Constants
+import logging
 
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+
+from config import Constants
 from urls import APP_URLS
 
 
 app = Application(APP_URLS, debug=Constants.DEBUG)
 
 if __name__ == '__main__':
-    print(f'[i] Debug mode: {Constants.DEBUG}')
-    print(f'[i] Running at port: {Constants.PORT}')
-    app.listen(Constants.PORT)
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.info(f'[i] Debug mode: {Constants.DEBUG}')
+    logging.info(f'[i] Running at port: {Constants.PORT}')
 
+    app.listen(Constants.PORT)
     IOLoop.current().start()
