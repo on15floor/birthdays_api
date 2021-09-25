add3ab4a94851ea35435c0432625599a554a5b93
on15floor <on15floor@gmail.com>
2021-09-25 18:59:35 +0300

    Add some tests

--- web.py
+++ web.py
@@ -7,12 +7,14 @@ from config import Constants
 from urls import APP_URLS
 
 
-app = Application(APP_URLS, debug=Constants.DEBUG)
+def make_app():
+    return Application(APP_URLS, debug=Constants.DEBUG)
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
     logging.info(f'[i] Debug mode: {Constants.DEBUG}')
     logging.info(f'[i] Running at port: {Constants.PORT}')
-
+    app = make_app()
     app.listen(Constants.PORT)
     IOLoop.current().start()
